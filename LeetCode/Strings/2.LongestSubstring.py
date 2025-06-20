e8573b5745a2935be02e923cfdaf2a660d689664
Anjali Thawait <64317446+anjalitwt009@users.noreply.github.com>
2025-06-20 21:36:52 +0100

    Add files via upload

---
+++ LeetCode/Strings/2.LongestSubstring.py
@@ -0,0 +1,52 @@
+
+# Medium
+
+# Given a string s, find the length of the longest
+
+# without duplicate characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ 
+
+# Constraints:
+
+#     0 <= s.length <= 5 * 104
+#     s consists of English letters, digits, symbols and spaces.
+
+def lengthOfLongestSubstring(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    
+    c = set()
+    res=0
+    n=len(s)
+    l=0
+    for i in range(n):
+        while s[i] in c:
+            c.remove(s[l])
+            l = l+1
+        c.add(s[i])
+        res = max(res,i-l+1)
+    return res
